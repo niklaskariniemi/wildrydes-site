6e92577f9d45472d804b99a0f01015215e0e620d
niklaskariniemi <93410047+niklaskariniemi@users.noreply.github.com>
2024-11-25 13:15:02 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_bkXsmMwwJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7ohf6b81esa8o5bjt2ok78l48a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_0mJJpL7sy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2iohkbt8ca2c8dot3qmgeedq3f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
