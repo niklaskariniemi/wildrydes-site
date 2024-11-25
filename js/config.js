dca82102b20ed7da5f5360794bbe61360e022876
niklaskariniemi <93410047+niklaskariniemi@users.noreply.github.com>
2024-11-25 12:44:41 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_bkXsmMwwJ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7ohf6b81esa8o5bjt2ok78l48a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
